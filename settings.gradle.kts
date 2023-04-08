3ef290d7810564cd880a1863579e749d05125084
Jonas Gorauskas <jgorauskas@gmail.com>
2023-04-07 22:01:35 -0800

    build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,29 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.api.internal.FeaturePreviews.Feature.TYPESAFE_PROJECT_ACCESSORS
+import org.gradle.api.internal.FeaturePreviews.Feature.VERSION_CATALOGS
+
+enableFeaturePreview(TYPESAFE_PROJECT_ACCESSORS.name)
+enableFeaturePreview(VERSION_CATALOGS.name)
 
 rootProject.name = "DataStructuresByHand"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+    includeBuild("build-logic")
+}
+
+gradle.rootProject {
+    val projectGroup: String by settings
+    val projectVersion: String by settings
+
+    allprojects {
+        group = projectGroup
+        version = projectVersion
+        description = "data structures by hand"
+    }
+}
